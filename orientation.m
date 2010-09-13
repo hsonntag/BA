3b5d0693131fa63ed5826d15d7ca15096328c36d
Hermann <hermann.sonntag@gmail.com>
2010-09-13 19:11:29 +0000

    formatted diagram

--- orientation.m
+++ orientation.m
@@ -1,7 +1,6 @@
 function [ angle ] = orientation( orig_vec, rec_vec )
 %ORIENTATION angle between two vectors
-%   ORIENTATION(O,R) returns the angle, in degrees, of the two vectors with
-    real elements.
+%   ORIENTATION(O,R) returns the angle, in degrees, of the two vectors with real elements.
 
 angle = acosd(dot(rec_vec,orig_vec)/(sqrt(sum(rec_vec.^2)*sum(orig_vec.^2))));
 end
