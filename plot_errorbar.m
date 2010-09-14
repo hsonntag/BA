6b00aa7873ba6c7a9b2cde86e37079763f41a1fa
Hermann <hermann.sonntag@gmail.com>
2010-09-14 19:40:01 +0000

    completed captions

--- plot_errorbar.m
+++
@@ -1,9 +0,0 @@
-function [  ] = plot_errorbar( X,Y,E,LineSpec )
-%PLOT_ERRORBAR Summary of this function goes here
-%   Detailed explanation goes here
-hold on;
-plot(X,Y,LineSpec);
-stem(X,Y+E,LineSpec);
-stem(X,Y-E,LineSpec);
-end
-
